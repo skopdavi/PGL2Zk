19cf48f2050315a3f5f779883ed501b5c4c7575f
David Škop <skop.david@gmail.com>
2018-09-14 22:15:13 +0200

    Updt

--- PGL2Zk/Helpers/GlobalHelper.cs
+++ PGL2Zk/Helpers/GlobalHelper.cs
@@ -8,8 +8,8 @@ namespace PGL2Zk.Helpers
 {
     public static class GlobalHelper
     {
-        public static string UserName { get; set; } = "st82779";
+        public static string UserName { get; set; } = "";
 
-        public static string Password { get; set; } = "Crackisbest0";
+        public static string Password { get; set; } = "";
     }
 }
